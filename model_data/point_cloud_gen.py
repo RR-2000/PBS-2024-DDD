37c9f44b1f82f5789f5950a33225e28c6b8e9632
rrajaraman <rrajaraman@ethz.ch>
2024-11-27 18:24:51 +0100

    Added Tub PC

--- model_data/point_cloud_gen.py
+++ model_data/point_cloud_gen.py
@@ -3,17 +3,18 @@ import numpy as np
 import pyrender
 
 # Load the mesh from an OBJ file
-file_name = 'duck'
+file_name = 'tub'
 mesh = trimesh.load(f'{file_name}.obj')
 
 # Sample points on the surface of the mesh
-num_points = 10000  # Adjust this to sample more or fewer points
+num_points = 20000  # Adjust this to sample more or fewer points
 sampled_points, face_indices = mesh.sample(num_points, return_index=True)
 with open(f'{file_name}.xyz', 'w') as f:
     f.write('x y z\n')  # Optional header
     for point in sampled_points:
         f.write(f'{point[0]:.6f},{point[1]:.6f},{point[2]:.6f}\n')
 
+scale = 0.5
 # Display or save the point cloud (optional)
-point_cloud = trimesh.points.PointCloud(sampled_points)
+point_cloud = trimesh.points.PointCloud(sampled_points*scale)
 point_cloud.show()  # Opens an interactive viewer if supported
